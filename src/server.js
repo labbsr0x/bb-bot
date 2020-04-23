14bec4e292fa7e79a41c4f0081a0246a58c70bcb
C1313471 Gustavo Rodrigues Coelho <c1313471@interno.bb.com.br>
2020-04-23 09:59:16 -0300

    Api test
    Signed-off-by: C1313471 Gustavo Rodrigues Coelho <c1313471@interno.bb.com.br>

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const openshiftRestClient = require('openshift-rest-client').OpenshiftClient;
+const { KubeConfig } = require('kubernetes-client')
+const Request = require('kubernetes-client/backends/request')
+const Client = require('kubernetes-client').Client
+const k8s = require('@kubernetes/client-node');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -192,6 +197,91 @@ app.post("/add/version", async (req, res) => {
       res.status(400).json({
           "status": "Err"
       })
+    }
+})
+
+app.get("/list/deployments", async (req, res) => {
+    const cluster = {
+        name: 'arq3.0',
+        server: 'http://caas.nuvem.bb.com.br/v3',
+    };
+
+    const user = {
+        name: 'c1313471',
+        password: '01281012',
+    };
+
+    const context = {
+        name: 'my-context',
+        user: user.name,
+        cluster: cluster.name,
+    };
+
+    const kc = new k8s.KubeConfig();
+    kc.loadFromOptions({
+        clusters: [cluster],
+        users: [user],
+        contexts: [context],
+        currentContext: context.name,
+    });
+    const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+    k8sApi.listNamespacedPod('ath-barimetrics')
+    .then((res) => {
+	    console.log(res.body);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+    let apps = await db.listApps(req.query)
+    res.status(200).json({
+        "status": "OK",
+        "result": apps
+    })
+})
+
+app.get("/list/namespaces", async (req, res) => {
+    try {
+        const kubeconfig = new KubeConfig()
+
+        // kubeconfig.loadFromString(JSON.stringify(config))
+        kubeconfig.loadFromFile('/Users/gustavocoelho/projetos/bb-bot/src/config')
+        
+        const backend = new Request({ kubeconfig })
+        const client = new Client({backend, version: '1.13'})
+        const namespaces = await client.api.v1.namespaces.get()
+        console.log('namespace', namespaces);
+        res.status(200).json({
+            "status": "OK",
+            "result": namespaces
+        })
+    } catch (err) {
+        console.log('error', err)
+    }
+})
+
+app.get("/deploy/promster/:app", async (req, res) => {
+    try {
+        const kubeconfig = new KubeConfig()
+        kubeconfig.loadFromFile('/Users/gustavocoelho/projetos/bb-bot/src/config')
+
+        // kubeconfig.loadFromString(JSON.stringify(config))
+        const deploymentManifest = require('./deployments/bb-promster.json')
+        deploymentManifest.metadata.name = req.params.app
+        console.log('deployment', deploymentManifest)
+        const backend = new Request({ kubeconfig })
+        const client = new Client({backend, version: '1.13'})
+        const namespaces = await client.api.v1.namespaces.get()
+        console.log('namespace', namespaces);
+        
+        
+        await client.apis.apps.v1.namespaces('barimetrics').deployments.post({ body: deploymentManifest })
+        const deployment = await client.apis.apps.v1.namespaces('default').deployments(deploymentManifest.metadata.name).get()
+        res.status(200).json({
+            "status": "OK",
+            "result": deployment
+        })
+    } catch (err) {
+        console.log('error', err)
     }
 })
 
