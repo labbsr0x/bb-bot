890dc1381ed99d48f3cd8d73967d588a2f10949c
Gustavo Coelho <gutorc@hotmail.com>
2020-05-06 11:19:11 -0300

    Add eslint
    Adding eslint to project and fixing lint problems.
    
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- src/environment.js
+++ src/environment.js
@@ -1,37 +1,35 @@
-const ETCD_URLS = process.env.ETCD_URLS;
-const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
-let LANGUAGE = process.env.LANGUAGE;
-let BB_BOT_KB_CONFIG = process.env.BB_BOT_KB_CONFIG;
+const ETCD_URLS = process.env.ETCD_URLS
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
+let LANGUAGE = process.env.LANGUAGE
+let BB_BOT_KB_CONFIG = process.env.BB_BOT_KB_CONFIG
 
 /***
  * Load all envs and structure them correctly
  * @returns {{ETCD_URLS: string[]}}
  */
-function loadEnvs() {
-    if (!ETCD_URLS) {
-        throw "ETCD_URLS cannot be null or empty";
-    }
+const loadEnvs = () => {
+  if (!ETCD_URLS) {
+    throw new Error('ETCD_URLS cannot be null or empty')
+  }
 
-    if (!TELEGRAM_TOKEN) {
-        throw "TELEGRAM_TOKEN cannot be null or empty";
-    }
+  if (!TELEGRAM_TOKEN) {
+    throw new Error('TELEGRAM_TOKEN cannot be null or empty')
+  }
 
-    if (!LANGUAGE) {
-        LANGUAGE = 'en'
-    }
+  if (!LANGUAGE) {
+    LANGUAGE = 'en'
+  }
 
-    if (!BB_BOT_KB_CONFIG) {
-      BB_BOT_KB_CONFIG = null
-    }
+  if (!BB_BOT_KB_CONFIG) {
+    BB_BOT_KB_CONFIG = null
+  }
 
-    return {
-        'ETCD_URLS': ETCD_URLS.split(","),
-        'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
-        'LANGUAGE': LANGUAGE,
-        'KB_CONFIG': BB_BOT_KB_CONFIG
-    };
-};
+  return {
+    ETCD_URLS: ETCD_URLS.split(','),
+    TELEGRAM_TOKEN: TELEGRAM_TOKEN,
+    LANGUAGE: LANGUAGE,
+    KB_CONFIG: BB_BOT_KB_CONFIG
+  }
+}
 
-
-module.exports = loadEnvs();
-
+module.exports = loadEnvs()
--- src/routers/kbRouters.js
+++ src/routers/kbRouters.js
@@ -1,76 +1,73 @@
 import express from 'express'
-import {App} from '../types/App'
-import {Settings} from '../types/Settings'
+import { App } from '../types/App'
+import { Settings } from '../types/Settings'
 
-const util = require('util')
-const { KB_CONFIG } = require('../environment');
+const { KB_CONFIG } = require('../environment')
 const { KubeConfig } = require('kubernetes-client')
 const Request = require('kubernetes-client/backends/request')
 const Client = require('kubernetes-client').Client
 
-let router = express.Router()
+const router = express.Router()
 
-router.get("/list/namespaces", async (req, res) => {
+router.get('/list/namespaces', async (req, res) => {
   try {
-      const kubeconfig = new KubeConfig()
-      console.log('kb config', KB_CONFIG)
-      // kubeconfig.loadFromString(JSON.stringify(config))
-      kubeconfig.loadFromFile(KB_CONFIG)
-      
-      const backend = new Request({ kubeconfig })
-      const client = new Client({backend, version: '1.13'})
-      const namespaces = await client.api.v1.namespaces.get()
-      console.log('namespace', namespaces);
-      res.status(200).json({
-          "status": "OK",
-          "result": namespaces
-      })
+    const kubeconfig = new KubeConfig()
+    console.log('kb config', KB_CONFIG)
+    // kubeconfig.loadFromString(JSON.stringify(config))
+    kubeconfig.loadFromFile(KB_CONFIG)
+
+    const backend = new Request({ kubeconfig })
+    const client = new Client({ backend, version: '1.13' })
+    const namespaces = await client.api.v1.namespaces.get()
+    console.log('namespace', namespaces)
+    res.status(200).json({
+      status: 'OK',
+      result: namespaces
+    })
   } catch (err) {
-      console.log('error', err)
+    console.log('error', err)
   }
 })
 
-router.post("/deploy/promster", async (req, res) => {
+router.post('/deploy/promster', async (req, res) => {
   try {
-      const kubeconfig = new KubeConfig()
-      kubeconfig.loadFromFile(KB_CONFIG)
+    const kubeconfig = new KubeConfig()
+    kubeconfig.loadFromFile(KB_CONFIG)
 
-      // kubeconfig.loadFromString(JSON.stringify(config))
-      let settings = new Settings()
-      settings.setNamespace('barimetrics')
-      let app = new App(req.body.app)
-      let deploy = app.getManifest()
-      const backend = new Request({ kubeconfig })
-      const client = new Client({backend, version: '1.13'})
-      // // const namespaces = await client.api.v1.namespaces.get()
-      // // console.log('namespace', namespaces);
-      
-      
-      await client.apis.apps.v1.namespaces(app.getNamespace(settings)).deployments.post({ body: deploy })
-      const deployment = await client.apis.apps.v1.namespaces(app.getNamespace(settings)).deployments(deploy.metadata.name).get()
-      res.status(200).json({
-          "status": "OK",
-          "result": deployment
-      })
+    // kubeconfig.loadFromString(JSON.stringify(config))
+    const settings = new Settings()
+    settings.setNamespace('barimetrics')
+    const app = new App(req.body.app)
+    const deploy = app.getManifest()
+    const backend = new Request({ kubeconfig })
+    const client = new Client({ backend, version: '1.13' })
+    // // const namespaces = await client.api.v1.namespaces.get()
+    // // console.log('namespace', namespaces);
+
+    await client.apis.apps.v1.namespaces(app.getNamespace(settings)).deployments.post({ body: deploy })
+    const deployment = await client.apis.apps.v1.namespaces(app.getNamespace(settings)).deployments(deploy.metadata.name).get()
+    res.status(200).json({
+      status: 'OK',
+      result: deployment
+    })
   } catch (err) {
-      console.log('error', err)
+    console.log('error', err)
   }
 })
 
-
-router.get("/check/deploy/:app", async (req, res) => {
+router.get('/check/deploy/:app', async (req, res) => {
   try {
-      const kubeconfig = new KubeConfig()
-      kubeconfig.loadFromFile(KB_CONFIG)
-      const backend = new Request({ kubeconfig })
-      const client = new Client({backend, version: '1.13'})
-      const deployment = await client.apis.apps.v1.namespaces('barimetrics').deployments(req.params.app).get()
-      res.status(200).json({
-          "status": "OK",
-          "result": deployment
-      })
+    const kubeconfig = new KubeConfig()
+    kubeconfig.loadFromFile(KB_CONFIG)
+    const backend = new Request({ kubeconfig })
+    const client = new Client({ backend, version: '1.13' })
+    const deployment = await client.apis.apps.v1.namespaces('barimetrics').deployments(req.params.app).get()
+    res.status(200).json({
+      status: 'OK',
+      result: deployment
+    })
   } catch (err) {
-      console.log('error', err)
+    console.log('error', err)
   }
 })
 export default router
