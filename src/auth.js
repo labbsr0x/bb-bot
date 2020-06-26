1a8a954030a86dda3e5bc8876c3a8018985a0f48
Karine Valença <valenca.karine@gmail.com>
2020-06-26 11:20:06 -0300

    finish middleware to check if authentication is valid

--- src/auth.js
+++ src/auth.js
@@ -1,21 +1,28 @@
+const { BASIC_AUTH_USERNAME, BASIC_AUTH_PASSWORD } = require('./environment')
 
 function isValidAuth (req, res, next) {
 	const authorizationHeader = req.get('Authorization')
 	const token = authorizationHeader.split(' ')[1]
-	const text = Buffer.from(token, 'base64').toString('ascii')
-	console.log(text)
-	return next()
+	// const textToken = Buffer.from(token, 'base64').toString('ascii')
+	const validToken = simpleAuth()
+	if (token === validToken) {
+		return next()
+	} else {
+		res.status(400).json({
+			status: 'Error',
+			message: 'Invalid authorization'
+		})
+	}
 }
 
 function simpleAuth (req, res, next) {
 	console.log('simple auth')
-	const user = 'user'
-	const password = 'password'
+	const user = BASIC_AUTH_USERNAME
+	const password = BASIC_AUTH_PASSWORD
 	const buff = Buffer.from(user + ':' + password)
 	const encodeString = buff.toString('base64')
 
-	res.set('Authorization', 'Basic ' + encodeString)
-	res.send(encodeString)
+	return encodeString
 }
 
 module.exports = {
--- src/environment.js
+++ src/environment.js
@@ -2,6 +2,8 @@ const ETCD_URLS = process.env.ETCD_URLS
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
 let LANGUAGE = process.env.LANGUAGE
 let BB_BOT_KB_CONFIG = process.env.BB_BOT_KB_CONFIG
+const BASIC_AUTH_USERNAME = process.env.BASIC_AUTH_USERNAME
+const BASIC_AUTH_PASSWORD = process.env.BASIC_AUTH_PASSWORD
 
 /***
  * Load all envs and structure them correctly
@@ -28,7 +30,9 @@ const loadEnvs = () => {
 		ETCD_URLS: ETCD_URLS.split(','),
 		TELEGRAM_TOKEN: TELEGRAM_TOKEN,
 		LANGUAGE: LANGUAGE,
-		KB_CONFIG: BB_BOT_KB_CONFIG
+		KB_CONFIG: BB_BOT_KB_CONFIG,
+		BASIC_AUTH_USERNAME: BASIC_AUTH_USERNAME,
+		BASIC_AUTH_PASSWORD: BASIC_AUTH_PASSWORD
 	}
 }
 
