9374eb0ee8e7aa5a0a317458fa5497dc2b221499
Karine Valença <valenca.karine@gmail.com>
2020-06-26 10:57:31 -0300

    improved auth

--- src/auth.js
+++ src/auth.js
@@ -1,18 +1,21 @@
 
 function isValidAuth (req, res, next) {
-	console.log('is valid auth')
+	const authorizationHeader = req.get('Authorization')
+	const token = authorizationHeader.split(' ')[1]
+	const text = Buffer.from(token, 'base64').toString('ascii')
+	console.log(text)
 	return next()
 }
 
-function simpleAuth (req, res) {
+function simpleAuth (req, res, next) {
 	console.log('simple auth')
 	const user = 'user'
 	const password = 'password'
 	const buff = Buffer.from(user + ':' + password)
 	const encodeString = buff.toString('base64')
 
-	res.set('WWW-Authenticate', encodeString)
-	res.send('set auth')
+	res.set('Authorization', 'Basic ' + encodeString)
+	res.send(encodeString)
 }
 
 module.exports = {
